b425c5a983a2893895f0f95deac69635797419c3
doot <gugegby@gmail.com>
2021-06-30 03:39:16 +0800

    add register files.

--- src/main/scala/glec2/RegFile.scala
+++ src/main/scala/glec2/RegFile.scala
@@ -3,16 +3,39 @@ package glec2
 import spinal.core._
 import spinal.lib._
 
+case class RegFileIO(implicit val conf: GlecCoreParams) extends Bundle {
+  val rs1_addr = in UInt(5 bits)
+  val rs1_data = out Bits(conf.xlen bits)
+
+  val rs2_addr = in UInt(5 bits)
+  val rs2_data = out Bits(conf.xlen bits)
+
+  val dm_addr = in UInt(5 bits)
+  val dm_rdata = out Bits(conf.xlen bits)
+  val dm_wdata = in Bits(conf.xlen bits)
+  val dm_en = in Bool()
+
+  val waddr = in UInt(5 bits)
+  val wdata = in Bits(conf.xlen bits)
+  val wen = in Bool()
+}
 
 case class RegFile(implicit val conf : GlecCoreParams) extends Component {
-  val io = new Bundle {
-    val rs1_addr = in UInt(conf.xlen bits)
-    val rs1_data = out Bits(conf.xlen bits)
-    val rs2_addr = in UInt(conf.xlen bits)
-    val rs2_data = out Bits(conf.xlen bits)
-    val dm_addr = in UInt(conf.xlen bits)
-    val dm_data = out UInt(conf.xlen bits)
+  val io = RegFileIO()
+
+  val regFile = Mem(Bits(conf.xlen bits), 32) randBoot()
+
+  when(io.wen && (io.waddr =/= U"0")) {
+    regFile(io.waddr) := io.wdata
+  };
+  
+  when(io.dm_en && (io.dm_addr =/= U"0")) {
+    regFile(io.dm_addr) := io.dm_wdata
   }
+
+  io.rs1_data := Mux(io.rs1_addr =/= U"0", regFile(io.rs1_addr), B"0");
+  io.rs2_data := Mux(io.rs2_addr =/= U"0", regFile(io.rs2_addr), B"0");
+  io.dm_rdata := Mux(io.dm_addr =/= U"0", regFile(io.dm_addr), B"0");
 }
 
 
