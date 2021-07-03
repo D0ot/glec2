b72c9a845533ce88949a3669d06c76351ca3b041
doot <gugegby@gmail.com>
2021-07-03 13:19:46 +0800

    update design graph

--- src/main/scala/glec2/CoreParams.scala
+++ src/main/scala/glec2/CoreParams.scala
@@ -4,11 +4,12 @@ case class CoreParams(xlen : Int, rv64: Boolean){
   val name: String = "GLEC2"
   // currently it is just used as memory
   val l1cacheSize = 8192;
+
   val d1cacheSize = 8192;
-  
+
   val pcWidth = 32;
 
   val pcInitVal = 0;
 
-  val bypass = false
+  val bypass = true; 
 }
